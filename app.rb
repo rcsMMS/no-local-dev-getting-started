75a677c10561e178511164a8f5f5b893a3a6abb9
Ryan Spring <ryan.spring@t-systems-mms.com>
2016-05-10 11:02:36 +0200

    Update app.rb
    Added code to view binder

--- app.rb
+++ app.rb
@@ -9,9 +9,13 @@ get "/" do
   erb :home
 end
 
-
 class Contact < ActiveRecord::Base
   self.table_name = 'salesforce.contact'
+end
+
+get "/contacts" do
+  @contacts = Contact.all
+  erb :index
 end
 
 #get "/contacts" do
